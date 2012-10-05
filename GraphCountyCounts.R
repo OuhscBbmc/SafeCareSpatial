33e07d1a92390419e48049b8f10a8a83f4a3b370
Will Beasley <wibeasley@hotmail.com>
2012-10-05 16:32:24 -0500

    more simple map

--- GraphCountyCounts.R
+++ GraphCountyCounts.R
@@ -18,7 +18,18 @@ pathInputDirectory <- "F:/Projects/OuHsc/SafeCare/Spatial/SafeCareSpatial/PhiFre
 pathInputSummaryCounty <- file.path(pathInputDirectory, "CountCounty.csv")
 pathInputSummaryCountyYear <- file.path(pathInputDirectory, "CountCountyYear.csv")
 
-dvName <- "Unemployment2012June"
+dvName <- "Count"
 titleTopPlot <- "Unemployment\n2012 June"
 legendTopPlot <- "Percentage Rates\n(Divided into Quartiles)"
 
+ds <- read.csv(pathInputSummaryCounty)
+stateMap <- map("county", "oklahoma", fill=TRUE, col="transparent", plot=FALSE)
+countyIDs <- seq_along(stateMap$names)
+
+sp <- map2SpatialPolygons(stateMap, IDs=countyIDs,  proj4string=CRS(" +proj=longlat +datum=NAD83 +ellps=GRS80 +towgs84=0,0,0"))
+sp <- SpatialPolygonsDataFrame(sp, data=ds)
+
+labelCoordinates <- coordinates(sp)
+labelCoordinates[c(1:77)[ds$CountyName=="Pottawatomie"],2] <- coordinates(ok_poly_sp2)[c(1:77)[ds$CountyName=="Pottawatomie"],2] + .1
+breaksQuartile <- quantile(ds[, dvName])#quantile(ds[, dvName], seq(0,1,length=5))
+breaksDecile <- quantile(ds[, dvName], seq(from=0, to=1, length=11))#quantile(ds[, dvName], seq(0,1,length=5))
