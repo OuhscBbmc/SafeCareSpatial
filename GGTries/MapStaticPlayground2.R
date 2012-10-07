180389e2202784c282fefadaa0f34cebdb4c0d98
Will Beasley <wibeasley@hotmail.com>
2012-10-07 13:14:43 -0500

    After alighning alphabetizing; Before reversing color

--- GGTries/MapStaticPlayground2.R
+++ GGTries/MapStaticPlayground2.R
@@ -21,46 +21,45 @@ require(plyr)
 pathInputDirectory <- "F:/Projects/OuHsc/SafeCare/Spatial/SafeCareSpatial/PhiFreeDatasets"
 pathInputSummaryCounty <- file.path(pathInputDirectory, "CountCountyFortified.csv")
 
+# dvName <- "CountPerCapita"
+# roundedDigits <- 2
+dvName <- "CountPerCapitaRank"
+roundedDigits <- 0
 
-dvName <- "CountPerCapita"
-# dvName <- "CountPerCapitaRank"
-dsValueAllVariables <- read.csv(pathInputSummaryCounty)
-dsValue <- data.frame(CountyID=dsValueAllVariables$CountyID, CountyName=tolower(dsValueAllVariables$CountyName), DV=dsValueAllVariables[, dvName])
-# dsValue$DV <- dsValue[, dvName]
+dsValueAllVariables <- read.csv(pathInputSummaryCounty, stringsAsFactors=FALSE)
+dsValue <- data.frame(CountyID=dsValueAllVariables$CountyID, CountyNameLower=tolower(dsValueAllVariables$CountyName), CountyName=dsValueAllVariables$CountyName, DV=dsValueAllVariables[, dvName], stringsAsFactors=FALSE)
 
 dsBoundary <- map_data(map="county", region="OK")
 dsBoundary$region <- dsBoundary$subregion
-# countyIDs <- seq_along(unique(dsBoundary$region))
 
 dsCenterPoint <- map("county", "oklahoma", fill=TRUE, col="transparent", plot=FALSE)
-countyIDs <- seq_along(dsCenterPoint$names)
+# countyIDs <- seq_along(dsCenterPoint$names) # cbind(seq_along(dsCenterPoint$names), order(dsCenterPoint$names))
+countyIDs <- order(dsCenterPoint$names) #Using the 'order' fx accounts for the different alphabetical schemes
 spForCenters <- map2SpatialPolygons(dsCenterPoint, IDs=countyIDs,  proj4string=CRS(" +proj=longlat +datum=NAD83 +ellps=GRS80 +towgs84=0,0,0"))
-# sp <- SpatialPolygonsDataFrame(spForCenters, data=dsValue)
+spForCenters <- SpatialPolygonsDataFrame(spForCenters, data=dsValue)
+
 labelCoordinates <- coordinates(spForCenters)
+labelCoordinates[which(dsValue$CountyName=="Pottawatomie"), 2] <- coordinates(spForCenters)[which(dsValue$CountyName=="Pottawatomie"), 2] + .1
+labelCoordinates[which(dsValue$CountyName=="Love"), 2] <- coordinates(spForCenters)[which(dsValue$CountyName=="Love"), 2] + .05
 colnames(labelCoordinates) <- c("long", "lat")
+
 dsValue <- cbind(dsValue, labelCoordinates)
-
-
-# dsBoundary <- map("county", "oklahoma", fill=TRUE, col="transparent", plot=FALSE)
-# countyIDs <- seq_along(dsBoundary$names)
-# sp <- map2SpatialPolygons(dsBoundary, IDs=countyIDs,  proj4string=CRS(" +proj=longlat +datum=NAD83 +ellps=GRS80 +towgs84=0,0,0"))
-#  sp <- SpatialPolygonsDataFrame(sp, data=dsValue)
+rm(labelCoordinates, spForCenters, dsCenterPoint)
 
 # spFortified <- fortify(sp, region="ID")
 
-g <- ggplot(dsValue, aes(map_id=CountyName)) 
+g <- ggplot(dsValue, aes(map_id=CountyNameLower)) 
 g <- g + geom_map(aes(fill=DV), map=dsBoundary, color="gray20")
-#g <- g + geom_map(aes(fill=DV), map=spFortified, color="gray20") 
-g <- g + geom_text(aes(label=CountyName, x=long, y=lat)) 
-                       
-                       
+#g <- g + geom_text(aes(label=CountyName, x=long, y=lat)) 
+g <- g + geom_text(aes(label=CountyName, x=long, y=lat), vjust=-.2, size=4)
+g <- g + geom_text(aes(label=round(DV, roundedDigits), x=long, y=lat), vjust=1)
+
 g <- g + expand_limits(x=dsBoundary$long, y=dsBoundary$lat) 
-# g <- g + expand_limits(x=dsBoundary$x, y=dsBoundary$y)
 g <- g + scale_fill_gradient(name=dvName)
 g <- g + coord_map()
 g <- g + theme(axis.text.x=element_blank(), axis.text.y=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank(), axis.ticks.length=unit(0, "cm"))# + theme(
 g <- g + theme(plot.background=element_blank(), panel.background=element_blank())
 g <- g + theme(legend.position=c(0,0), legend.justification=c("left","bottom"))
-g <- g + theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) #+ theme(panel.margin = unit(c(0, 0, 0, 0), "cm"))
+g <- g + theme(plot.margin=unit(c(0, 0, 0, 0), "cm")) #+ theme(panel.margin = unit(c(0, 0, 0, 0), "cm"))
 print(g)
 
