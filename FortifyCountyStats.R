6f76b04840a679cb5dc6daa2f4284e9ab9e39fd5
Will Beasley <wibeasley@hotmail.com>
2012-10-09 10:39:38 -0500

    Fortified with yearly census population, not 2012 pop

--- FortifyCountyStats.R
+++ FortifyCountyStats.R
@@ -4,7 +4,8 @@ require(maps)
 require(maptools)
 
 pathDirectory <- "F:/Projects/OuHsc/SafeCare/Spatial/SafeCareSpatial/PhiFreeDatasets"
-pathInputCensus <- file.path(pathDirectory, "PopByCounty-2012Aug.csv")
+pathInputCensus2012 <- file.path(pathDirectory, "PopByCounty-2012Aug.csv")
+pathInputCensusYearly <- file.path(pathDirectory, "OklahomaCountyPopulationsByYear.csv")
 pathInputCountyLookup <- file.path(pathDirectory, "CountyLookups.csv")
 pathInputSummaryCounty <- file.path(pathDirectory, "CountCounty.csv")
 pathInputSummaryCountyYear <- file.path(pathDirectory, "CountCountyYear.csv")
@@ -12,18 +13,19 @@ pathOutputSummaryCounty <- file.path(pathDirectory, "CountCountyFortified.csv")
 pathOutputSummaryCountyYear <- file.path(pathDirectory, "CountCountyYearFortified.csv")
 
 #Read in the necessary data files
-dsCensus <- read.csv(pathInputCensus, stringsAsFactors=FALSE)
+dsCensus2012 <- read.csv(pathInputCensus2012, stringsAsFactors=FALSE)
+dsCensusYearly <- read.csv(pathInputCensusYearly, stringsAsFactors=FALSE)
 dsLookup <- read.csv(pathInputCountyLookup, stringsAsFactor=F)
 dsCounty <- read.csv(pathInputSummaryCounty, stringsAsFactors=FALSE)
 dsCountyYear <- read.csv(pathInputSummaryCountyYear, stringsAsFactors=FALSE)
 
-dsCensus <- dsCensus[, c("CountyName", "PopTotal")] #Drop the unnecessary columns
+dsCensus2012 <- dsCensus2012[, c("CountyName", "PopTotal")] #Drop the unnecessary columns
 years <- sort(unique(dsCountyYear$MsurYear))
 yearCount <- length(years)
 ################################################################################################
 ### Work on dsCounty
 ################################################################################################
-dsCounty <- merge(x=dsCounty, y=dsCensus, by="CountyName")
+dsCounty <- merge(x=dsCounty, y=dsCensus2012, by="CountyName")
 dsCounty$CountPerCapitaAnnual <- (dsCounty$Count / dsCounty$PopTotal) / yearCount
 dsCounty$CountRank <- rank(dsCounty$Count)
 dsCounty$CountPerCapitaRank <- rank(dsCounty$CountPerCapitaAnnual)
@@ -33,7 +35,7 @@ dsCenterPoint <- map("county", "oklahoma", fill=TRUE, col="transparent", plot=FA
 # countyIDs <- seq_along(dsCenterPoint$names) # cbind(seq_along(dsCenterPoint$names), order(dsCenterPoint$names))
 countyIDs <- order(dsCenterPoint$names) #Using the 'order' fx accounts for the different alphabetical schemes
 spForCenters <- map2SpatialPolygons(dsCenterPoint, IDs=countyIDs,  proj4string=CRS(" +proj=longlat +datum=NAD83 +ellps=GRS80 +towgs84=0,0,0"))
-# spForCenters <- SpatialPolygonsDataFrame(spForCenters, data=dsCensus)
+# spForCenters <- SpatialPolygonsDataFrame(spForCenters, data=dsCensus2012)
 labelCoordinates <- coordinates(spForCenters)
 labelCoordinates[which(dsCounty$CountyName=="Pottawatomie"), 2] <- coordinates(spForCenters)[which(dsCounty$CountyName=="Pottawatomie"), 2] + .1
 labelCoordinates[which(dsCounty$CountyName=="Love"), 2] <- coordinates(spForCenters)[which(dsCounty$CountyName=="Love"), 2] + .05
@@ -59,14 +61,18 @@ for( year in years ) {
   dsSlice$MsurYear <- year #This fills in the NAs that exist in the county's without a report that year.
   dsSlice <- plyr::rename(dsSlice, replace=c(ID="CountyID")) #Rename the ID column
   dsSlice <- dsSlice[, colnames(dsSlice) != "Name"] #Drop the redundant (County)Name column.
-#   dsSlice$LabelLongitude <- 0
-#   dsSlice$LabelLatitude <- 0
   
   dsSlice <- cbind(dsSlice, labelCoordinates)
   dsCountyYearFortified <- rbind(dsCountyYearFortified, dsSlice)
 }
 
-dsCountyYearFortified <- merge(x=dsCountyYearFortified, y=dsCensus, by="CountyName")
+dsCensusYearly$YearPlusOne <- dsCensusYearly$Year + 1
+dsCensusYearly <- dsCensusYearly[, colnames(dsCensusYearly) != "Year"]
+#dsCountyYearFortified <- merge(x=dsCountyYearFortified, y=dsCensus, by="CountyName")
+dsCountyYearFortified <- merge(x=dsCountyYearFortified, y=dsCensusYearly, 
+                               by.x=c("CountyName", "MsurYear"), 
+                               by.y=c("CountyName", "YearPlusOne"))
+
 dsCountyYearFortified$CountPerCapitaAnnual <- dsCountyYearFortified$Count / dsCountyYearFortified$PopTotal
 dsCountyYearFortified$CountRank <- rank(dsCountyYearFortified$Count)
 dsCountyYearFortified$CountPerCapitaRank <- rank(dsCountyYearFortified$CountPerCapitaAnnual)
