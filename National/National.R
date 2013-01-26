d766f8c0666d8c85c2681134b05c90a5529afc9d
Will Beasley <wibeasley@hotmail.com>
2013-01-26 14:46:38 -0600

    Uses abbreviations.

--- National/National.R
+++ National/National.R
@@ -6,16 +6,12 @@ library(maps)
 library(maptools)
 # windows() #If you want to pop out the window
 
+#Read in the three necessary datasets.
 dsPolygon = map_data('state')
-ds <- read.csv("National/Data/FakeData.csv")
+ds <- read.csv("National/Data/FakeData.csv") #Change this name.
+dsLabels <- read.csv("National/Data/StateLabels.csv", stringsAsFactors=FALSE)
 
-#Write a fake dataset to help with structure
-# ds <- data.frame(StateName = unique(dsPolygon$region), CountPerCapita = runif(runif(49, 0, 1)))
-# ds$CountPerCapita[-sample(1:49, 10)] <- NA
-# write.csv(ds, "National/Data/FakeData.csv", row.names=FALSE)
-
-
-#Drat the real map
+#Draw the real map
 g <- ggplot(ds, aes(map_id = StateName)) 
 g <- g + geom_map(aes(fill=CountPerCapita), colour='grey90', map=dsPolygon, size=.1)
 g <- g + scale_fill_continuous(na.value = "grey75") 
@@ -26,13 +22,9 @@ g <- g + theme(plot.background=element_blank(), panel.background=element_blank()
 g <- g + theme(legend.position=c(.8,.05), legend.justification=c("left","bottom")) #Move the legend
 g <- g + theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) #Remove blank border
 
-# The next six lines are just for writing labels on the non-missing states.  Labels may be unnecessary.
-dsStatesForLabeles <- map('state', fill=TRUE, col="transparent", plot=FALSE)
-stateNamesForLabels <- sapply(strsplit(dsStatesForLabeles$names, ":"), function(x) x[1])
-statePolygonsForLabels <- map2SpatialPolygons(dsStatesForLabeles, IDs=stateNamesForLabels, proj4string=CRS("+proj=longlat +datum=wgs84"))
-stateLabel <- data.frame(StateName = unique(stateNamesForLabels), coordinates(statePolygonsForLabels))
-dsSelectedNames <- stateLabel[stateLabel$StateName %in% ds$StateName[!is.na(ds$CountPerCapita)], ]
-g <- g + geom_text(data=dsSelectedNames, aes(X1, X2, label=StateName), size=3  )
+# The next two lines are just for writing labels on the non-missing states. Labels may be unnecessary.
+dsLabels <- dsLabels[dsLabels$StateName %in% dsLabels$StateName[!is.na(ds$CountPerCapita)], ]
+g <- g + geom_text(data=dsLabels, aes(X, Y, label=Abbreviation), size=3, colour="gray30")
 
 g #Draw it to the window, but looking at the png (written below) is more important.
 
---
+++ National/UtilitiesForCreatingFlow.R
@@ -0,0 +1,43 @@
+#Starting example is from https://groups.google.com/forum/?fromgroups=#!topic/ggplot2/Fw8WOZV72SQ
+rm(list=ls(all=TRUE))
+library(ggplot2)
+library(plyr)
+library(maps)
+library(maptools)
+# windows() #If you want to pop out the window
+
+dsPolygon = ggplot2::map_data('state')
+
+
+######################################
+### Write a fake dataset to help with structure
+######################################
+
+
+dsStateDV <- data.frame(StateName = unique(dsPolygon$region), CountPerCapita=NA_integer_, Proportion=NA_real_)
+dsStateDV$CountPerCapita <-  rpois(49, lambda=100)
+dsStateDV$CountPerCapita[-sample(1:49, 10)] <- NA
+
+dsStateDV$Proportion <-  runif(runif(49, 0, 1)) #I don't know why the example uses two 'runif's.
+dsStateDV$Proportion[-sample(1:49, 10)] <- NA
+
+write.csv(dsStateDV, "National/Data/FakeData.csv", row.names=FALSE)
+
+
+
+######################################
+### Create dataset for labels
+######################################
+
+# The next six lines are just for writing labels on the non-missing states.  Labels may be unnecessary.
+dsStatesForLabeles <- maps::map('state', fill=TRUE, col="transparent", plot=FALSE)
+stateNamesForLabels <- sapply(strsplit(dsStatesForLabeles$names, ":"), function(x) x[1])
+statePolygonsForLabels <- maptools::map2SpatialPolygons(dsStatesForLabeles, IDs=stateNamesForLabels, proj4string=CRS("+proj=longlat +datum=wgs84"))
+dsStateLabels <- data.frame(StateName = unique(stateNamesForLabels), coordinates(statePolygonsForLabels))
+dsStateLabels$Abbreviations <- c("AL", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "ID", "IL", "IN", "IO", "KA", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NH", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VY", "VA", "WA", "WV", "WI", "WY")
+dsStateLabels <- plyr::rename(dsStateLabels, replace=c("X1"="X", "X2"="Y"))
+# write.csv(dsStateLabels, "National/Data/StateLabels.csv", row.names=FALSE)
+
+
+# dsLabels <- read.csv("National/Data/StateLabels.csv", stringsAsFactors=FALSE)
+# paste(dsLabels$Abbreviation, collapse="\", \"")
