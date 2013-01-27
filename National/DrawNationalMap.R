b1d18606e7a84aa0d305bf396fa5fafcaeae2f46
Will Beasley <wibeasley@hotmail.com>
2013-01-26 22:10:20 -0600

    Dynamic title

--- National/DrawNationalMap.R
+++ National/DrawNationalMap.R
@@ -27,26 +27,26 @@ years <- sort(unique(ds$Year))
 colorLow <- hcl(h=45, c=20, l=90)
 colorHigh <- hcl(h=25, c=60, l=50)
 
-range(dsPolygon$long)
-range(dsPolygon$lat)
+xLimits <- c(-122.5, -70) #range(dsPolygon$long)
+yLimits <- c(26, 48.3) #range(dsPolygon$lat)
 
 # for( year in years ) {
   year <- 2010
   dsYear <- ds[ds$Year==year, ]
+  plotTitle <- paste0(dvName, ": ", year)
   #Draw the real map
   g <- ggplot(dsYear, aes(map_id = StateName)) 
   g <- g + geom_map(aes_string(fill=dvName), colour='grey90', map=dsPolygon, size=.1)
   g <- g + scale_fill_continuous(na.value = "grey75", low=colorLow , high=colorHigh, name="", guide=guide_colorbar()) 
-  g <- g + expand_limits(x=c(-122.5, -70), y=c(26, 48.3))
-#   g <- g + xlim(c(-123, -67.00742)) + ylim(c(25.12993, 49.38323))
+  g <- g + expand_limits(x=xLimits, y=yLimits)
   g <- g + coord_map() #Set the correct aspect ratio.
   g <- g + theme(axis.text.x=element_blank(), axis.text.y=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank(), axis.ticks.length=unit(0, "cm")) #Turn of coordinates
 #   g <- g + theme(plot.background=element_blank(), panel.background=element_blank()) #Clear background grid
   g <- g + theme(legend.position=c(.8,1.05), legend.justification=c("right","top")) #Move the legend
   g <- g + theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) #Remove blank border
-  g <- g + labs(main="dssd", x="xxxx")
 
   g <- g + theme(legend.background = element_rect(fill=NA))
+  g <- g + annotate("text", x=xLimits[1], y=yLimits[1], label=plotTitle, hjust=0, vjust=0)
 
   
 
@@ -57,8 +57,8 @@ range(dsPolygon$lat)
   g #Draw it to the window, but looking at the png (written below) is more important.
   
   width <- 10 #Set this to the page/slide width
-  aspectRatio <- .52 #Keep this fixed.
+  aspectRatio <- .54 #Keep this fixed.
   resolution <- 600 #The boundaries are 200 for drafts and 1200 for publications.  Watch out, b/c it throws off the legend proportion.
   
-  ggsave("National/NationalMap.png", plot=g,  width=width, height=width*aspectRatio, units="in", dpi=resolution)
+  ggsave("National/NationalMap.png", plot=g,  width=width, height=width*aspectRatio, units="in", dpi=resolution)#, bg="pink")
 # }
