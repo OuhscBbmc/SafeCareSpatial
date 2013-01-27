745b5967e5200c41657871c67911f851faf65174
Will Beasley <wibeasley@hotmail.com>
2013-01-26 21:43:46 -0600

    Cleaned up utilities a litte; and renamed to DrawNationalMap.R

--- National/UtilitiesForCreatingFlow.R
+++ National/UtilitiesForCreatingFlow.R
@@ -5,16 +5,11 @@ library(plyr)
 library(maps)
 library(maptools)
 library(spdep)
-# windows() #If you want to pop out the window
-
-dsPolygon = ggplot2::map_data('state')
-
 
 ######################################
 ### Write a fake dataset to help with structure
 ######################################
-
-
+dsPolygon = ggplot2::map_data('state')
 dsStateDV <- data.frame(StateName = unique(dsPolygon$region), CountPerCapita=NA_integer_, Proportion=NA_real_)
 dsStateDV$CountPerCapita <-  rpois(49, lambda=100)
 dsStateDV$CountPerCapita[-sample(1:49, 10)] <- NA
@@ -22,10 +17,7 @@ dsStateDV$CountPerCapita[-sample(1:49, 10)] <- NA
 dsStateDV$Proportion <-  runif(runif(49, 0, 1)) #I don't know why the example uses two 'runif's.
 dsStateDV$Proportion[-sample(1:49, 10)] <- NA
 
-write.csv(dsStateDV, "National/Data/FakeData.csv", row.names=FALSE)
-
-
-
+# write.csv(dsStateDV, "National/Data/FakeData.csv", row.names=FALSE)
 ######################################
 ### Create dataset for labels
 ######################################
@@ -37,15 +29,15 @@ statePolygonsForLabels <- maptools::map2SpatialPolygons(dsStatesForLabeles, IDs=
 dsStateLabels <- data.frame(StateName = unique(stateNamesForLabels), coordinates(statePolygonsForLabels))
 dsStateLabels$Abbreviations <- c("AL", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "ID", "IL", "IN", "IO", "KA", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NH", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VY", "VA", "WA", "WV", "WI", "WY")
 dsStateLabels <- plyr::rename(dsStateLabels, replace=c("X1"="X", "X2"="Y"))
+
 # write.csv(dsStateLabels, "National/Data/StateLabels.csv", row.names=FALSE)
 
+# dsLabels <- read.csv("National/Data/StateLabels.csv", stringsAsFactors=FALSE)
+# paste(dsLabels$Abbreviation, collapse="\", \"")
 
 ######################################
 ### Create neighbors object (nb) with labels
 ######################################
-stateNBForLabels <- poly2nb(statePolygonsForLabels)
-adjMatrix <- nb2mat(hh,style="B") #check that adjacency matrix can be created
+stateNBForLabels <- spdep::poly2nb(statePolygonsForLabels)
+adjMatrix <- spdep::nb2mat(hh,style="B") #check that adjacency matrix can be created
 
-
-# dsLabels <- read.csv("National/Data/StateLabels.csv", stringsAsFactors=FALSE)
-# paste(dsLabels$Abbreviation, collapse="\", \"")
